d49eaac146ae43c20d25190c70abe3ab181b3e56
jake howlett <jake.howlett93@gmail.com>
2021-04-21 17:28:27 +0100

    integrated giphy-api and finished search functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import giphy from 'giphy-api';
 
 import './App.css';
 import Search from './components/search';
@@ -8,10 +9,20 @@ import GifList from './components/gif_list';
 function App() {
   const [ gifList, setGifList ] = useState(["3LdNdcLDaPL8bK0ObS"])
 
+  const searchGifs = (value) => {
+    const ids = [];
+    giphy("9LtO6Ka1X6PP9yhQhm45g8H9nBK6Hyyo").search(value).then((result) => {
+      result.data.forEach((gif) => {
+        ids.push(gif.id);
+      });
+      setGifList(ids);
+    });
+  }
+
   return (
     <div>
       <div className="left-scene">
-        <Search />
+        <Search search={searchGifs} />
         <div className="selected-gif">
           <Gif id="3LdNdcLDaPL8bK0ObS" />
         </div>
--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Search = () => {
+const Search = ({search}) => {
+
+  const handleChange = (event) => {
+    search(event.target.value)
+  }
+
   return (
-    <input type="text" className="form-search" />
+    <input type="text" className="form-search" onChange={handleChange} />
   );
 }
 
